0d02dccbf1a4e2cc24ce8cdb741a8012ee75c50c
Michael Wiederhold <michael.wiederhold@luminari.de>
2022-03-28 10:53:32 +0200

    Adds game board generation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello Typescript!")
+import { Map, List } from "immutable";
+
+enum GameState {
+    NextPlayer,
+    Win,
+    Draw,
+};
+
+const players = (idx: number): string => {
+    return ["X", "O"][idx];
+}
+
+const generateBoard = (): List<List<string>> => {
+    const row = List(["", "", ""]);
+    return List([row, row, row]);
+};
+
+// Well, this is awkward...
+const initialize = (): Map<string, any> => {
+    return Map({
+        board: generateBoard(),
+        state: Map({
+            gameState: GameState.NextPlayer,
+            player: 0,
+        }),
+    });
+};
